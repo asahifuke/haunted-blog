7aee9d4aaaba9016eab4668c5efef6f0970de90e
asahifuke <asahi.fuke@medpeer.co.jp>
2022-08-22 15:34:39 +0900

    rubocopを通しました

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Blog < ApplicationRecord
+  before_save :cannot_set_up_random_eyecatch_if_not_premium
+
   belongs_to :user
   has_many :likings, dependent: :destroy
   has_many :liking_users, class_name: 'User', source: :user, through: :likings
@@ -10,12 +12,16 @@ class Blog < ApplicationRecord
   scope :published, -> { where('secret = FALSE') }
 
   scope :search, lambda { |term|
-    where("title LIKE ? OR content LIKE ?", "%#{term}%", "%#{term}%")
+    where('title LIKE ? OR content LIKE ?', "%#{term}%", "%#{term}%")
   }
 
   scope :default_order, -> { order(id: :desc) }
 
   def owned_by?(target_user)
     user == target_user
+  end
+
+  def cannot_set_up_random_eyecatch_if_not_premium
+    self.random_eyecatch = false unless user.premium
   end
 end
